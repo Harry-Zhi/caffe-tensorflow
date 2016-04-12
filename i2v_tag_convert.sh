fdbc88a40b3ddb5f99bebaed46b88337b89abd8c
pkm <parag@pkmital.com>
2016-04-12 10:56:36 -0700

    example scripts for converting i2v and vgg

---
+++ i2v_tag_convert.sh
@@ -0,0 +1,1 @@
+python3 convert.py models/i2v-tag/illust2vec_tag.prototxt models/i2v-tag/illust2vec_tag_ver200.caffemodel models/i2v-tag/illust2vec_tag.npy models/i2v-tag/illust2vec_tag.py
---
+++ vgg-face-convert.sh
@@ -0,0 +1,1 @@
+python3 convert.py models/vgg-face/vgg-face-deploy.prototxt models/vgg-face/vgg-face.caffemodel models/vgg-face/vgg_face.npy models/vgg-face/vgg_face.py
